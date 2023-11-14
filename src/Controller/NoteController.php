18918b992dc77424e239c6985f1c1f480c0c1a95
Dylan <63360083+czyrok@users.noreply.github.com>
2023-11-14 10:00:03 +0100

    Ajout des dernières pages

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -9,8 +9,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class NoteController extends AbstractController
 {
     #[Route('/explore', name: 'app_explore')]
-    public function index(): Response
+    public function explore(): Response
     {
         return $this->render('note/explore.html.twig');
+    }
+
+    #[Route('/search', name: 'app_search')]
+    public function search(): Response
+    {
+        return $this->render('note/search.html.twig');
+    }
+
+    #[Route('/note/{noteId}', 'app_note_one')]
+    public function view(int $noteId): Response {
+        return $this->render('note/view.html.twig');
+    }
+
+    #[Route('/private/{privateLink}', 'app_private_one')]
+    public function private(int $privateLink): Response {
+        return $this->render('note/view.html.twig');
     }
 }
